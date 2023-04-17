612d6319dc8408713d5ffdd61b1de65ca0b93f0c
trolly4427g <yendrickgameplays@gmail.com>
2023-04-17 08:21:39 -0400

    Diseño responsive
    para iphone 7/8/9 plus entre otros

--- Menu Inicio/js/menu.js
+++ Menu Inicio/js/menu.js
@@ -2,5 +2,11 @@
 function aparece() {
     let apare=document.getElementsByClassName('menuico')[0];
     apare.classList.toggle('show');
-    let menu=document.getElementsByTagName
+    let menu=document.querySelectorAll('.mostrar');
+    for (let i = 0; i < menu.length; i++) {
+        menu[i].classList.toggle('menu');
+        
+    }
+
+    
 }
