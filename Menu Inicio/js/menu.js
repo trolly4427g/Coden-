7db937c066b0606b553d753164d0b63aaaaf739e
trolly4427g <yendrickgameplays@gmail.com>
2023-04-20 00:14:09 -0400

    1.2.3
    reparacion de js

--- Menu Inicio/js/menu.js
+++ Menu Inicio/js/menu.js
@@ -1,12 +1,8 @@
-
 function aparece() {
-    let apare=document.getElementsByClassName('menuico')[0];
-    apare.classList.toggle('show');
-    let menu=document.querySelectorAll('.mostrar');
-    for (let i = 0; i < menu.length; i++) {
-        menu[i].classList.toggle('menu');
-        
-    }
-
-    
+  let apare = document.getElementsByClassName("menuico")[0];
+  apare.classList.toggle("show");
+  let menu = document.querySelectorAll(".mostrar");
+  for (let i = 0; i < menu.length; i++) {
+    menu[i].classList.toggle("menu");
+  }
 }
